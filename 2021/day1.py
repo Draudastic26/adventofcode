2b7df7b4e9516ec5024e0022ed3f446cdef2f3bf
Eduard Reger <eduard.reger@gmail.com>
2021-12-17 16:36:48 +0100

    🚀 Day 1 - Part 2

--- 2021/day1.py
+++ 2021/day1.py
@@ -6,10 +6,20 @@ with open(input_path) as file:
     content = file.readlines()
     content = [int(line.rstrip()) for line in content]
 
-count = 0
+count1 = 0
 for i in range(1, len(content)):
     if(content[i] > content[i-1]): 
-        count += 1
+        count1 += 1
 
-print("Day 1")
-print(count)
+
+print(f"Part 1: {str(count1)}")
+
+window = 3
+count2 = 0
+for i in range(len(content) - window):
+    sumFirst = sum(content[i: i + window])
+    sumSecond = sum(content[i + 1: i + 1 + window])
+    if(sumSecond > sumFirst): 
+        count2 += 1
+
+print(f"Part 2: {str(count2)}")
